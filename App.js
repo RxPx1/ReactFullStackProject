558d9da2fd4e51e0c61998282ce7e3030fa464e7
RxPx <110516770+RxPx1@users.noreply.github.com>
2022-12-14 23:35:56 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <div>
+        <div>
+          Your Todo's
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed dog</td>
+                <td>Rob</td>
+              </tr>
+              <th scope='row'>2</th>
+              <td>Get haircut</td>
+              <td>Rob</td>
+            </tbody>
+          </table>
+        </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
